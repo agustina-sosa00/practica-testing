6d9892960853419b94a08f5594a0e72bdf078608
Agustina Sosa <agustinaayelensosa2@gmail.com>
2025-04-03 14:44:03 -0300

    exercise: filterEvenNumbers and removeDuplicates

--- src/ejercicios/removeDuplicates.js
+++ src/ejercicios/removeDuplicates.js
@@ -1,8 +1,10 @@
+export const removeDuplicate = (array) => {
+  const newArr = new Set(array);
+  return [...newArr];
+};
+
 // export const removeDuplicate = (array) => {
-//   const newArr = new Set(array);
-//   return [...newArr];
+//   return [...new Set(array)];
 // };
 
-export const removeDuplicate = (array) => {
-  return [...new Set(array)];
-};
+// la clase Set se utiliza para eliminar duplicados. Se lo puede utilizar con un array de numeros, strings y cualquier tipo de dato que sea comparable. Su funcionamiento es: mapea el array internamente y devuelve todos los valores unicos, es decir, que no se repiten.
